1a2516722b0e709a836220930178cb009a29d915
yyp <591848210@qq.com>
2023-10-17 22:29:30 +0800

    first commit

---
+++ src/main/java/org/example/service/CfsService.java
@@ -0,0 +1,12 @@
+package org.example.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CfsService {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello spring boot!";
+    }
+}
